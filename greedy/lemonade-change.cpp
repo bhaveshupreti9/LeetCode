face8a02b3d657bd0c6b076724c4529b5ffc44a5
bhaveshupreti9 <bhaveshuk0402@gmail.com>
2026-06-17 10:56:27 +0530

    LC-lemonade-change [Runtime: N/A; Memory: N/A]

--- greedy/lemonade-change.cpp
+++ greedy/lemonade-change.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
         int ans = 0;
-        int d1=0,d2=0,d3=0;
+        int five=0,d2=0,d3=0;
         int n = bills.size();
         for(int i=0;i<n;i++){
-            if(bills[i] == 5)d1++;
+            if(bills[i] == 5)five++;
             else if(bills[i] == 10){
                 d2++;
-                if(d1>0)d1--;
+                if(five>0)five--;
                 else return false;
             }
             else{
-                if(d1 > 0 && d2 >0){
+                if(five > 0 && d2 >0){
                     d3++;
-                    d1--;
+                    five--;
                     d2--;
                 }
-                else if(d2 <=0 && d1 > 2){
-                    d1-=3;
+                else if(d2 <=0 && five > 2){
+                    five-=3;
                     d3++;
                 }
                 else return false;
